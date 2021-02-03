a1085ad6e03cf7c1e506c77741f5f0e5c7bb8a47
MatiasStorm <mat-435@hotmail.com>
2021-02-03 16:03:09 +0100

    Improve print format

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import java.time.temporal.Temporal;
 import java.util.ArrayList;
 
 public class Main {
@@ -10,9 +11,9 @@ public class Main {
         //Blive stort og stærkt engang kører vi en masse linjer kode igen og igen uden grund
 
         String str = "";
-       Member member1 = new Member(true, "Morten", "130195-1303");
+        String divider = "***************************************************";
         ArrayList<Person> fitnessList = new ArrayList<>();
-        fitnessList.add(member1);
+        fitnessList.add(new Member(true, "Morten", "130195-1303"));
         fitnessList.add(new Member(false, "Martin", "221175-1011"));
         fitnessList.add(new Member(true, "Martina", "050970-1410"));
         fitnessList.add(new Member(false, "Marcel", "221175-1011"));
@@ -22,13 +23,19 @@ public class Main {
         fitnessList.add(new Instructor("Henning", "011081-1015", 15, 199));
 
         //Prints employees
-        System.out.println("Name  CPR     Hours Salary Vacation");
+        System.out.println("Name      CPR           Hours  Salary  Vacation");
+        System.out.println(divider);
         for (Person person: fitnessList) {
            str = "";
             if (person instanceof Employee) {
-                str += person.getName() + " " + person.getCpr() + " " + ((Employee) person).getHours() + " " + ((Employee) person).getSalary();
-                if (person instanceof Administration) {
-                   str += " " + ((Administration) person).getVacation();
+                Employee employee = (Employee) person;
+                str += employee.getName() + " ".repeat(10 - employee.getName().length());
+                str += employee.getCpr() + "     ";
+                str += employee.getHours() + "   ";
+                str += employee.getSalary() + "   ";
+                if (employee instanceof Administration) {
+                    Administration admin = (Administration) employee;
+                    str += admin.getVacation();
                 }
                 System.out.println(str);
             }
